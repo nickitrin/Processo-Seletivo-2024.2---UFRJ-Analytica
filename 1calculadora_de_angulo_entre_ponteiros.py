bbb6ef8bd1132e7873e8b502217e17b5d7cfa9fc
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-29 17:02:09 -0300

    Update 1calculadora_de_angulo_entre_ponteiros.py

--- 1calculadora_de_angulo_entre_ponteiros.py
+++ 1calculadora_de_angulo_entre_ponteiros.py
@@ -21,8 +21,7 @@ while True:
                     print (f"O menor ângulo é de {abs(360 - menorangulo)}°")
                 else:
                     print (f"O menor ângulo é de {abs(menorangulo)}°")
-            if int(horas[:2]) != -0:
-                print ("Input Inválido")
+
             else:
                 print ("Input Inválido")
     except Exception:
