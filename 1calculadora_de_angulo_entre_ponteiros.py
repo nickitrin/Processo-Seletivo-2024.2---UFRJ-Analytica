703363ccd5e05da7bb380bc9f0c4023e03a0fe47
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-29 17:10:31 -0300

    Update 1calculadora_de_angulo_entre_ponteiros.py

--- 1calculadora_de_angulo_entre_ponteiros.py
+++ 1calculadora_de_angulo_entre_ponteiros.py
@@ -8,7 +8,7 @@ while True:
             if 0<=int(horas[:2]) < 12 and int(horas[:2])< 24 and int(horas[3:])<60 and horas[2]==":":
                 angulohoras = int(horas[:2])*30
                 angulomin = int(horas[3:])*6
-                menorangulo = angulohoras - angulomin
+                menorangulo = abs(angulohoras - angulomin)
                 if menorangulo > 180 :
                     print (f"O menor ângulo é de {abs(360 - menorangulo)}°")
                 else:
@@ -16,14 +16,14 @@ while True:
             if int(horas[:2]) >= 12 and int(horas[:2])< 24 and int(horas[3:])<60 and horas[2]==":":
                 angulohoras = (int(horas[:2])-12)*30
                 angulomin = int(horas[3:])*6
-                menorangulo = angulohoras - angulomin
+                menorangulo = abs(angulohoras - angulomin)
                 if menorangulo > 180 :
                     print (f"O menor ângulo é de {abs(360 - menorangulo)}°")
                 else:
                     print (f"O menor ângulo é de {abs(menorangulo)}°")
-
-            else:
+            if int(horas[:2]) not in range(23):
                 print ("Input Inválido")
+                
     except Exception:
         print ("Input Inválido")
         
