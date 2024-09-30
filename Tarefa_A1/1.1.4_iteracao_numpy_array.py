a098cb7994a1e8f1168391f9dffd6a2a1e61cf41
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-29 23:09:59 -0300

    Update 1.1.4_iteracao_numpy_array.py

--- Tarefa_A1/1.1.4_iteracao_numpy_array.py
+++ Tarefa_A1/1.1.4_iteracao_numpy_array.py
@@ -1,8 +1,8 @@
-#questão a
-
 import numpy as np
 
 array1 = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
+
+#Calcule a media dos valores usando iteracao.
 
 contador = 1
 soma = 0 
@@ -14,4 +14,4 @@ for i in array1:
 
 print (media)
 
-#questão b
+# Calcule o desvio padrao dos valores usando iteracao
