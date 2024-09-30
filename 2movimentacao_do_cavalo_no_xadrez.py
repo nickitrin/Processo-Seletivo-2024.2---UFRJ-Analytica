19c1ff3b1bb20e8a32852f0b4618f299925ac342
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-29 21:04:24 -0300

    Update 2movimentacao_do_cavalo_no_xadrez.py

--- 2movimentacao_do_cavalo_no_xadrez.py
+++ 2movimentacao_do_cavalo_no_xadrez.py
@@ -3,22 +3,22 @@ numeros_casas= list(range(1,9))
 casos_possiveis = [[-1,-2], [-1,2], [-2,-1], [-2,1], [1,-2], [1,2], [2,-1], [2,1]]
 
 while True:
-    casa = input("")
-    if casa == "f":
-        print("Fim...")
-        break
-    else:
-        letracasa_cavalo = letras_casas.index(casa[0])
-        numerocasa_cavalo = numeros_casas.index(int(casa[1]))
-        cavalo_casas_num=[]
-        cavalo_casas=[]
-        
-        for i in casos_possiveis:
-            if (letracasa_cavalo - i[0])>=0 and (numerocasa_cavalo - i[1])>=0:
-                cavalo_casas_num+= [[letracasa_cavalo - i[0], numerocasa_cavalo - i[1] ]]
-        print (cavalo_casas_num)
-        
-        for n in cavalo_casas_num:
-            cavalo_casas+= [[letras_casas[n[0]] , numeros_casas[n[1]]]]
-            print (cavalo_casas)
-
+    try:
+        casa = input("")
+        if casa == "f":
+            print("Fim...")
+            break
+        else:
+            letracasa_cavalo = letras_casas.index(casa[0])
+            numerocasa_cavalo = numeros_casas.index(int(casa[1]))
+            cavalo_casas_num=[]
+            cavalo_casas=[]
+            
+            for i in casos_possiveis:
+                if 0<=(letracasa_cavalo - i[0])<=7 and 0<=(numerocasa_cavalo - i[1])<=7:
+                    cavalo_casas_num+= [[letracasa_cavalo - i[0], numerocasa_cavalo - i[1] ]]
+                    
+            for n in cavalo_casas_num:
+                cavalo_casas+= [f"{letras_casas[n[0]]}{numeros_casas[n[1]]}"]
+                string_cavalo_casas = ', '.join(cavalo_casas)
+            print (string_cavalo_casas)
