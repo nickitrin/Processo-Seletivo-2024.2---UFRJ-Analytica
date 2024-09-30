a8b4f81ddcec269afd5e0538762cf597b9d4f505
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-29 21:30:14 -0300

    Update 4frequencia_de_numeros.py

--- 4frequencia_de_numeros.py
+++ 4frequencia_de_numeros.py
@@ -1,1 +1,13 @@
-
+dic={}
+while True:
+    numero = input("")
+    if numero == "f":
+        print("Fim...")
+        break
+    else:
+        for i in dic:
+            if numero == i :
+                dic[i]+=1
+        if numero not in dic:
+            dic[numero]= 1
+    print(dic.keys())
