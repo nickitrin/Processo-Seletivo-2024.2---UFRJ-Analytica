7225267e651686b929a98f90fed2bb87207a2fc8
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-10-01 18:23:47 -0300

    Update 3_Calcule_o IDH_medio_do_brasil_no_ano_de_1991.py

--- Tarefa_A1/3_Calcule_o IDH_medio_do_brasil_no_ano_de_1991.py
+++ Tarefa_A1/3_Calcule_o IDH_medio_do_brasil_no_ano_de_1991.py
@@ -10,8 +10,7 @@ df_filtrado = df[filtro]
 
 df_idhm = df_filtrado.loc[:, 'idhm']
 
-df_idhm = pd.to_numeric(df_idhm, errors='coerce')
-df_idhm = df_idhm.dropna()
 
 print (df_idhm)
 print (np.mean(df_idhm))
+
