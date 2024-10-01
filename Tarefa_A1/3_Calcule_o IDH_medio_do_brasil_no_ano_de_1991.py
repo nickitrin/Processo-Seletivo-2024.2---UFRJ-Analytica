844495be3b79bda83b93764826ee32a65ca28ca6
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-10-01 18:11:24 -0300

    Create 3_Calcule_o IDH_medio_do_brasil_no_ano_de_1991.py

---
+++ Tarefa_A1/3_Calcule_o IDH_medio_do_brasil_no_ano_de_1991.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+import numpy as np
+
+
+df = pd.read_csv("arquivo.csv")
+
+filtro = df["ano"] == 1991
+df_filtrado = df[filtro]
+
+df_idhm = df_filtrado.loc[:, 'idhm']
+
+df_idhm = pd.to_numeric(df_idhm, errors='coerce')
+df_idhm = df_idhm.dropna()
+
+print (df_idhm)
+print (np.mean(df_idhm)*1000)
