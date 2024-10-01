0ccdf15561a6553daa646672e39cef92117c5481
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-30 22:26:54 -0300

    Update 1.1.4_iteracao_numpy_array.py

--- Tarefa_A1/1.1.4_iteracao_numpy_array.py
+++ Tarefa_A1/1.1.4_iteracao_numpy_array.py
@@ -15,3 +15,12 @@ for i in array1:
 print (media)
 
 # Calcule o desvio padrao dos valores usando iteracao
+
+dp2=0
+contador = 1
+
+for i in array1:
+    dp2 = (i-media)**2+dp2
+    dp= (dp2/contador)**0.5
+    contador+=1
+print (dp)
