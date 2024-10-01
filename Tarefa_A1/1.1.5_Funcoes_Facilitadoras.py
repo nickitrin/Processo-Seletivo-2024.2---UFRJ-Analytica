308fff0e188d4d5d4d1a41051e421d43ca2653e3
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-30 22:40:23 -0300

    Update 1.1.5_Funcoes_Facilitadoras.py

--- Tarefa_A1/1.1.5_Funcoes_Facilitadoras.py
+++ Tarefa_A1/1.1.5_Funcoes_Facilitadoras.py
@@ -2,8 +2,23 @@ import numpy as np
 
 array1 = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
 
+
 # Ordene os elementos do array da questão anterior usando o método .sort() próprio do numpy.
 
 array1=np.sort(array1)
 
 print (array1)
+
+#Para saber as dimensões do array podemos use a propriedade .shape
+
+print (array1.shape)
+
+#Extraia a média de um array numpy utilizando o metodo .mean():
+
+print(np.mean(array1))
+
+#O mesmo pode ser feito para o desvio padrao, com .std(), maxima e mınima com .max() e .min(), alem de muitos outros metodos.
+
+print (np.std(array1))
+print (np.max(array1))
+print (np.min(array1))
