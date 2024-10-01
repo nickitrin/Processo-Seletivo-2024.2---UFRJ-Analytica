1beb052d9bb062a66f2276f452c3855cbae77b18
Nicole Trindade <45500884+nickitrin@users.noreply.github.com>
2024-09-30 22:32:53 -0300

    Create 1.1.5_Funcoes_Facilitadoras.py

---
+++ Tarefa_A1/1.1.5_Funcoes_Facilitadoras.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array1 = np.array([5.5, 3.8, 9, 7.5, 10.0, 9.9, 8.5])
+
+# Ordene os elementos do array da questão anterior usando o método .sort() próprio do numpy.
+
+array1=np.sort(array1)
+
+print (array1)
